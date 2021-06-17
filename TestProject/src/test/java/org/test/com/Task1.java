e3983d63c6c6ab5ddebb2632a5c1f34d180ee794
sasikumar02 <sasikrishna97@gmail.com>
2021-06-17 22:10:32 +0530

    First commit

---
+++ TestProject/src/test/java/org/test/com/Task1.java
@@ -0,0 +1,25 @@
+package org.test.com;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Task1 {
+	public static void main(String[] args) throws InterruptedException {
+	System.setProperty("webdriver.chrome.driver",
+			"C:\\Users\\sasikumar\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+
+	WebDriver driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://www.jcpenney.com/");
+	driver.findElement(By.xpath("(//div[@class='SecondaryNavigationPanel-menuItemLink'])[1]")).click();
+	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	driver.findElement(By.xpath("//a[text()='Breakfast in Bed']")).click();
+	Thread.sleep(3000);
+	driver.quit();
+	
+}
+
+}
