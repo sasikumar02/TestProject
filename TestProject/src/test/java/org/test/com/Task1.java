3a784d27b336f35fe9462169762e424b30499bab
sasikumar02 <sasikrishna97@gmail.com>
2021-06-17 22:13:47 +0530

    second commit

--- TestProject/src/test/java/org/test/com/Task1.java
+++ TestProject/src/test/java/org/test/com/Task1.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Task1 {
 	public static void main(String[] args) throws InterruptedException {
 	System.setProperty("webdriver.chrome.driver",
-			"C:\\Users\\sasikumar\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+			"C:\\Users\\sasikumar\\git\\TestProject\\TestProject\\Drivers\\chromedriver.exe");
 
 	WebDriver driver = new ChromeDriver();
 	driver.manage().window().maximize();
